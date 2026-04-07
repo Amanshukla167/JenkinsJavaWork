a144044dfa89a01180076581c52cb943170da313
Aman Shukla <amanshukla167@gmail.com>
2026-04-07 17:01:38 +0530

    added new line to check the jenkins paipline

--- src/main/java/com/Book/Spring/JenkinsSetupApplication.java
+++ src/main/java/com/Book/Spring/JenkinsSetupApplication.java
@@ -9,6 +9,7 @@ public class JenkinsSetupApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsSetupApplication.class, args);
 		System.out.println("this Spring boot project is running from the Jenkin.");
+		System.out.println("added new line in this code for the jenkins");
 	}
 
 }
