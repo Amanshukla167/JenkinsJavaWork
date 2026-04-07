c040834825ca96d8ab5f16ef2676688b19a1a2b7
Aman Shukla <amanshukla167@gmail.com>
2026-04-07 13:32:40 +0530

    frist commit for jenkins thingh

---
+++ src/main/java/com/Book/Spring/JenkinsSetupApplication.java
@@ -0,0 +1,14 @@
+package com.Book.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSetupApplication.class, args);
+		System.out.println("this Spring boot project is running from the Jenkin");
+	}
+
+}
---
+++ src/test/java/com/Book/Spring/JenkinsSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Book.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
